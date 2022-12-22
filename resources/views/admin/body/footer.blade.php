2bc587b8079add10ae12fad6dbaa2f753b75ff8a
Anuranga <jayan.anuranga@gmail.com>
2022-12-22 11:38:39 -0800

    master -first commit

---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="main-footer">
+    <div class="pull-right d-none d-sm-inline-block">
+        <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
+            <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0)">FAQ</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">www.ictjayan.com</a>
+            </li>
+        </ul>
+    </div>
+    &copy; 2023 <a href="#">Bsc(Hons)Information Technology & Management (2nd Upper) University Of Moratuwa, Master of Computer Science <br /></a> All Rights Reserved.
+</footer>
